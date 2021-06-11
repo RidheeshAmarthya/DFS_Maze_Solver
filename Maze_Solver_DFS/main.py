0583a2636326b846540138578ae2df42251887be
Ridheesh Amarthya <vridheesh@gmail.com>
2021-06-11 16:44:18 +0530

    Add files via upload

---
+++ Maze_Solver_DFS/main.py
@@ -0,0 +1,101 @@
+
+a = [
+    [0,0,1,0,0,0,1,0],
+    [0,0,1,0,0,0,1,0],
+    [0,0,1,0,1,1,0,1],
+    [0,1,1,1,0,0,1,0],
+    [0,0,0,1,0,0,0,0],
+    [0,1,0,0,0,1,0,1],
+    [0,1,1,1,1,0,0,1],
+    [1,1,0,0,0,1,0,1],
+    [1,1,0,0,0,0,0,0],
+]
+zoom = 20
+borders = 6
+start = 0,0
+end = 8,7
+
+def make_step(k):
+  for i in range(len(m)):
+    for j in range(len(m[i])):
+      if m[i][j] == k:
+        if i>0 and m[i-1][j] == 0 and a[i-1][j] == 0:
+          m[i-1][j] = k + 1
+        if j>0 and m[i][j-1] == 0 and a[i][j-1] == 0:
+          m[i][j-1] = k + 1
+        if i<len(m)-1 and m[i+1][j] == 0 and a[i+1][j] == 0:
+          m[i+1][j] = k + 1
+        if j<len(m[i])-1 and m[i][j+1] == 0 and a[i][j+1] == 0:
+           m[i][j+1] = k + 1
+
+def print_m(m):
+    for i in range(len(m)):
+        for j in range(len(m[i])):
+            print( str(m[i][j]).ljust(2),end=' ')
+        print()
+
+def draw_matrix(a,m, the_path = []):
+    for i in range(len(a)):
+        for j in range(len(a[i])):
+            color = (255, 255, 255)
+            r = 0
+            if a[i][j] == 1:
+                color = (0, 0, 0)
+            if i == start[0] and j == start[1]:
+                color = (0, 255, 0)
+                r = borders
+            if i == end[0] and j == end[1]:
+                color = (0, 255, 0)
+                r = borders
+
+    for u in range(len(the_path)-1):
+        y = the_path[u][0]*zoom + int(zoom/2)
+        x = the_path[u][1]*zoom + int(zoom/2)
+        y1 = the_path[u+1][0]*zoom + int(zoom/2)
+        x1 = the_path[u+1][1]*zoom + int(zoom/2)
+
+m = []
+for i in range(len(a)):
+    m.append([])
+    for j in range(len(a[i])):
+        m[-1].append(0)
+i,j = start
+m[i][j] = 1
+
+k = 0
+while m[end[0]][end[1]] == 0:
+    k += 1
+    make_step(k)
+    draw_matrix(a, m)
+
+
+i, j = end
+k = m[i][j]
+the_path = [(i,j)]
+while k > 1:
+  if i > 0 and m[i - 1][j] == k-1:
+    i, j = i-1, j
+    the_path.append((i, j))
+    k-=1
+  elif j > 0 and m[i][j - 1] == k-1:
+    i, j = i, j-1
+    the_path.append((i, j))
+    k-=1
+  elif i < len(m) - 1 and m[i + 1][j] == k-1:
+    i, j = i+1, j
+    the_path.append((i, j))
+    k-=1
+  elif j < len(m[i]) - 1 and m[i][j + 1] == k-1:
+    i, j = i, j+1
+    the_path.append((i, j))
+    k -= 1
+  draw_matrix(a, m, the_path)
+
+for i in range(10):
+    if i % 2 == 0:
+        draw_matrix(a, m, the_path)
+    else:
+        draw_matrix(a, m)
+
+print_m(m)
+print(the_path)
